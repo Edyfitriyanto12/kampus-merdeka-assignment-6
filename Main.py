05f1fa0223b13ee1ae6e5124da248be729b7aabb
Edy fitriyanto <edyfitriyant0o12@gmail.com>
2023-10-22 21:09:38 +0700

    menambahkan fungsi perkalian dan pembagian

--- Main.py
+++ Main.py
@@ -1,19 +1,7 @@
-from math_function import add
+from math_function import multiply, divide
 
+hasil_perkalian = multiply(10, 5)
+print("Hasil Perkalian:", hasil_perkalian)
 
-def main():
-
-    data_1 = int(input("masukkan input 1 :"))
-    data_2 = int(input("masukkan input 2 :"))
-    operator = input("masukkan operator :")
-
-    if operator == "+":
-        result = add(data_1, data_2)
-
-    print("{} {} {} = {} ".format(data_1, operator, data_2, result))
-
-
-if __name__ == "__main__":
-    print("Hello Main !")
-    main()
-
+hasil_pembagian = divide(10, 2)
+print("Hasil Pembagian:", hasil_pembagian)
--- Math_function.py
+++ Math_function.py
@@ -1,4 +1,7 @@
+def multiply(a, b):
+    return a * b
 
-def add(a, b):
-    return a + b
-
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Tidak bisa dibagi dengan nol")
+    return a / b
